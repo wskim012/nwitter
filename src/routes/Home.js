22a39fb49611558ccb9388203f92faa53d9e93f5
wskim012 <wskim012@gmail.com>
2022-03-29 18:59:06 +0900

    realtime nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,30 +1,41 @@
 import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
-import { addDoc, collection, serverTimestamp, getDocs, query} from "firebase/firestore";
+import { addDoc, collection, serverTimestamp, getDocs, query,onSnapshot,orderBy} from "firebase/firestore";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-  const getNweets = async () => {
-    const q = query(collection(dbService, "nweets"));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-        const nweetObject = {
-        ...doc.data(), //doc.data() is an object, ...doc.data() is the content of that object
-        id: doc.id,
-        };
-        setNweets((prev) => [nweetObject, ...prev]);
-    });
-  };
+//   const getNweets = async () => {
+//     const q = query(collection(dbService, "nweets"));
+//     const querySnapshot = await getDocs(q);
+//     querySnapshot.forEach((doc) => {
+//         const nweetObject = {
+//         ...doc.data(), //doc.data() is an object, ...doc.data() is the content of that object
+//         id: doc.id,
+//         };
+//         setNweets((prev) => [nweetObject, ...prev]);
+//     });
+//   };
   useEffect(() => {
-    getNweets();
-  }, []);
+    const q = query(
+        collection(dbService, "nweets"),
+        orderBy("createdAt", "desc")
+        );
+    onSnapshot(q, (snapshot) => {
+        const nweetArr = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+        setNweets(nweetArr);
+    });
+  },[]);
   const onSubmit = async (event) => {
     event.preventDefault();
     try{
         const docRef = await addDoc(collection(dbService, "nweets"),{
-            nweet,
+            text: nweet,
             createdAt: serverTimestamp(),
+            creatorId: userObj.uid,
         })
         console.log("Document written with ID: ", docRef.id);
     } catch (error){
@@ -54,7 +65,7 @@ const Home = () => {
       <div>
         {nweets.map((nweet) => (
           <div key={nweet.id}>
-            <h4>{nweet.nweet}</h4>
+            <h4>{nweet.text}</h4>
           </div>
         ))}
       </div>
