1f4ce727b8f1206dff2b17d1a32019194f91ff6a
wskim012 <wskim012@gmail.com>
2022-03-30 17:19:17 +0900

    update profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import AppRouter from "components/Router";
 import {authService} from "fbase";
+import { updateProfile } from "@firebase/auth";
 
 function App() {
   const [init, setInit] = useState(false);
@@ -8,15 +9,50 @@ function App() {
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(()=>{
     authService.onAuthStateChanged((user)=>{
-      user ? setUserObj(user) : setUserObj (null);
+      if(user){
+        
+        if(user.displayName === null) {
+          const name = user.email.split("@")[0];
+          user.displayName = name;
+        }
+        // setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => updateProfile(user, { displayName: user.displayName }),
+        });
+          
+        // setUserObj({
+        //   displayName: user.displayName,
+        //   uid: user.uid,
+        //   updateProfile: (args) => user.updateProfile(args),
+        // });
+      } else {
+        setUserObj (null);
+      }
       setInit(true);
     })
   },[]);
+  const refreshUser = () => {
+    console.log("refresh")
+    const user = authService.currentUser;
+    console.log(user)
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => updateProfile(user, { displayName: user.displayName }),
+      // updateProfile: (args) => user.updateProfile(args),
+    });
+  }
   return (
     <>
       {init ? (
         // <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
-        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)} 
+          userObj={userObj} 
+        />
       ) : (
         "Initializing..."
       )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,53 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useEffect, useState } from "react";
+import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
+import { collection, getDocs, query, where } from "@firebase/firestore";
+import { updateProfile } from "@firebase/auth";
 
-export default () => {
+export default ({ userObj, refreshUser }) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const onLogOutClick = async () => {
       const status = await authService.signOut();
       history.push("/");
+    }
+    const getMyNweets = async () =>{
+      const q = query(collection(dbService,"nweets"),where("creatorId","==",userObj.uid));
+      const querySnapShot = await getDocs(q);
+      // querySnapShot.forEach((doc)=>{
+      //   console.log(doc.id,"=>",doc.data());
+      // })
     };
+    const onChange = (event) => {
+      const {
+        target: {value},
+      } = event;
+      setNewDisplayName(value);
+    }
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      console.log(userObj)
+      if(userObj.displayName !== newDisplayName){
+        await updateProfile(authService.currentUser,{
+          displayName: newDisplayName,
+        })
+        refreshUser();
+      }
+    }
+    useEffect(()=>{
+      getMyNweets();
+    },[]);
     return (
       <>
+        <form onSubmit={onSubmit}>
+          <input
+            onChange={onChange}
+            type="text"
+            placeholder="Display name"
+            value={newDisplayName}
+          />
+          <input type="submit" value="Update Profile" />
+        </form>
         <button onClick={onLogOutClick}>Log Out</button>
       </>
     );
